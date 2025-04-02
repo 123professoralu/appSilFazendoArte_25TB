dfe6b54090f259fc2c97d4a682b91ba3a313d97c
1.23 Professora Lu <112901053+123professoralu@users.noreply.github.com>
2025-04-01 22:23:36 -0300

    Menu de navegação
    Menu de navegação.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,38 @@
 import React from "react";
 import { View } from "react-native";
 import { useFonts, Montserrat_400Regular } from "@expo-google-fonts/montserrat";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Sobre from "./src/telas/Sobre";
+
+//Configuração do Menu
+const Tab = createBottomTabNavigator();
+
+function Menu(){
+  return <Tab.Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, color, size})=>{
+                let iconName;
+
+                if(route.name==="Sobre"){
+                  iconName = focused
+                  ? 'paw'
+                  : 'paw-outline';
+                }
+                return <Ionicons name={iconName} color={color} size={size}/>
+              },
+              tabBarActiveTintColor: 'purple',
+              tabBarInactiveTintColor: 'gray',
+              headerShown: false,
+            })}>
+            <Tab.Screen name="Sobre" component={Sobre}/>
+            <Tab.Screen name="Produtos" component={Sobre}/>
+            <Tab.Screen name="Lista de Desejos" component={Sobre}/>
+            <Tab.Screen name="Perfil" component={Sobre}/>
+        </Tab.Navigator>
+}
 
 export default function App() {
 
@@ -13,5 +43,7 @@ export default function App() {
     return <View/>;
   }
 
-  return <Sobre />
+  return <NavigationContainer>
+            <Menu />
+        </NavigationContainer>
 }
