d9c060c61fcae082e45779e412b40c34db1cf047
1.23 Professora Lu <112901053+123professoralu@users.noreply.github.com>
2025-05-06 18:55:15 -0300

    Delete src/telas/Produto directory

--- src/telas/Produto/estilosProdutos.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-const estilosProdutos=StyleSheet.create({
-    corFundo: {
-        backgroundColor: "purple",
-    }
-});
-
-export default estilosProdutos;
