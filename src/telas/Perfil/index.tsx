127c051561f08e97c738c8e840435f4ae6c7e9ae
1.23 Professora Lu <112901053+123professoralu@users.noreply.github.com>
2025-08-26 22:14:20 -0300

    Add files via upload

--- src/telas/Perfil/index.tsx
+++ src/telas/Perfil/index.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Card} from 'react-native-paper';
-import { View, TextInput, TouchableOpacity } from "react-native";
+import { View, TextInput, TouchableOpacity, Alert} from "react-native";
 import { CameraView, CameraType, useCameraPermissions } from "expo-camera";
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Texto from '../../componentes/Texto'
 import styles from './estilosPerfil'
@@ -11,6 +12,25 @@ export default function Index(){
     
     const[facing, setFacing] = useState<CameraType>('back');
     const[permission, requestPermission] = useCameraPermissions();
+
+    //Informações do Perfil
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+
+    //Carrega os dados do perfil salvo
+    useEffect(()=>{
+        async function carregarPerfil(){
+            const perfilSalvo = await AsyncStorage.getItem('Perfil')
+            if(perfilSalvo) {
+                const perfil = JSON.parse(perfilSalvo)
+                setNome(perfil.nome)
+                setEmail(perfil.email)
+                setWhatsapp(perfil.whatsapp)
+            }
+        }
+        carregarPerfil()
+    }, [])
     
     //Se as permissões da câmera ainda estiverem carregando, exibe uma view vazia
     if(!permission) {
@@ -32,6 +52,14 @@ export default function Index(){
         setFacing(current=>(current === 'back' ? 'front' : 'back'));
     }
 
+    //Salvar informações do perfil
+    async function salvarPerfil(){
+        const perfil = {nome,email,whatsapp};
+        await AsyncStorage.setItem('Perfil', JSON.stringify(perfil));
+        Alert.alert('Perfil salvo com sucesso.');
+        console.log(perfil);
+    }
+
     return <View style={styles.container}>
                 <CameraView facing={facing} style={styles.camera}>
                     <View style={styles.cameraContainer}>
@@ -43,14 +71,21 @@ export default function Index(){
                 <Card mode='elevated' style={styles.cardContainer}>
                     <Card.Content>
                         <Texto style={styles.text}>Nome completo</Texto>
-                        <TextInput style={styles.input} placeholder="Hermione Granger"/>
+                        <TextInput style={styles.input} value={nome} onChangeText={setNome}/>
                         
                         <Texto style={styles.text}>E-Mail</Texto>
-                        <TextInput style={styles.input} placeholder="mione.granger@hogwarts.com"/>
+                        <TextInput style={styles.input} value={email} onChangeText={setEmail}/>
 
                         <Texto style={styles.text}>WhatsApp</Texto>
-                        <TextInput style={styles.input} placeholder="(11) 99999-7474" keyboardType="numeric"/>
+                        <TextInput style={styles.input} keyboardType="numeric" value={whatsapp} onChangeText={setWhatsapp}/>
                     </Card.Content>
+                    <Card.Actions>
+                        <TouchableOpacity style={styles.botao} onPress={salvarPerfil}>
+                            <Texto style={styles.textoBotao}>
+                                <Ionicons name="save" size={20} color="white"/> Salvar Perfil
+                            </Texto>
+                        </TouchableOpacity>
+                    </Card.Actions>
                 </Card>
         </View>
 }
